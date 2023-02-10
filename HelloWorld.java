c2fa5ee5be001e7f1350ce251d2fa50ddf0e259b
iAmBrams <39541385+i24Seven@users.noreply.github.com>
2023-02-10 11:12:17 +0530

    commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package First_Program;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World.");
+		
+		system.out.println("This is My First program");
+
+	}
+}
